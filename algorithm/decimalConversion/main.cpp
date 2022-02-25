41f7cbe5c7025afde296f33dda3123e35658dcbb
y-ytakahashi <ty.ytakahashi.work@gmail.com>
2022-02-25 22:39:58 +0900

    構文エラーを修正

--- algorithm/decimalConversion/main.cpp
+++ algorithm/decimalConversion/main.cpp
@@ -11,10 +11,17 @@ int main()
   while (N >= 1)
   {
     if (N % 2 == 0)
+    {
       Answer = "0" + Answer;
-      cout << N % 2 << endl;
+      cout << "N is" << N << "N % 2 equal 0 calc answer >> " << N % 2 << endl;
+    }
+
     if (N % 2 == 1)
+    {
       Answer = "1" + Answer;
+      cout << "N is" << N << "N % 2 equal 1  calc answer >> " << N % 2 << endl;
+    }
+
     N = N / 2;
   }
 
